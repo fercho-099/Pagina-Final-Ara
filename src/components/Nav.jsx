fcd6e943427ef46939031d779d38f72d36a01316
fercho-099 <69776793+fercho-099@users.noreply.github.com>
2025-11-21 11:35:37 -0300

    Na v

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,43 @@
-import React from 'react';   
- 
+import React, {useState} from 'react';   
+import '../styles/style.css';
+
 function Nav() {   
+    
+    const [isDropdownOpen, setIsDrowdownOpen] = useState(false);
+
+    const toggleDropdown = () =>{
+        setIsDrowdownOpen(!isDropdownOpen);
+    }
+
     return (   
-        <nav style={{ backgroundColor: "#333", color: "white", padding: 
-"10px" }}>   
-            <ul style={{ listStyle: "none", display: "flex", 
-justifyContent: "space-around", margin: 0 }}>   
-                <li><a href="#" style={{ color: "white", 
-textDecoration: "none" }}>Inicio</a></li>   
-                <li><a href="#" style={{ color: "white", 
-textDecoration: "none" }}>Acerca de Ara Maquinarias</a></li>  
-                <li><a href="#" style={{ color: "white", 
-textDecoration: "none" }}>Contacto</a></li>   
+        <nav className="Nav-container">   
+            <ul className="Ul-container">   
+                <li className="nav-item">
+                    <a href="#" onClick={toggleDropdown} style={{ color: "white", textDecoration: "none" }}>Inicio</a>
+                    <div className="dropdown">
+                        <button className="btn Inicio">
+                            <ul className={`dropdown-menu ${isDropdownOpen ? 'open' : ''}`}>
+                                <li>
+                                    <a href="#Link1" className="dropdown-link">Construccion</a>
+                                </li>
+                                <li>
+                                    <a href="#Link2" className="dropdown-link"> Jardineria</a>
+                                </li>
+                                <li>
+                                    <a href="#Link3" className="dropdown-link">Hogar</a>
+                                </li>
+                            </ul>
+                        </button>
+                    </div>
+                <li className="li-item">
+                    <a href="#" onClick={toggleDropdown}  style={{ color: "white", textDecoration: "none" }}>Acerca de Ara Maquinarias</a></li>  
+                <li className="li-item">
+                    <a href="#" onClick={toggleDropdown}  style={{ color: "white", textDecoration: "none" }}>Contacto</a></li>   
+            </li>
             </ul>   
         </nav>   
-    );   
+    );  
+                     
 }   
  
 export default Nav;   
