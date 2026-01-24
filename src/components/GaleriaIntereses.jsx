49b9f8c3adaea6f0af1267f091c450e0dd0bdbaf
fercho-099 <69776793+fercho-099@users.noreply.github.com>
2026-01-24 16:30:07 -0300

    Carrito de compra
    Carrito de compra

--- src/components/GaleriaIntereses.jsx
+++ src/components/GaleriaIntereses.jsx
@@ -1,7 +1,9 @@
-function GaleriaIntereses({ menuAra }) {  //por que intereses?
+function GaleriaIntereses({ menuAra }) {  //corresponde a ¿quienes somos? Valores Y Ofertas.
   function capturarClic(e) {
-    e.target.style.backgroundColor = '#ef8009ff';
-    e.target.style.color = 'white';
+    /*e.target.style.backgroundColor = '#ef8009ff';
+    e.target.style.color = 'white'; */   ///ver el evento para mas adelante
+
+    ///se deberia poner la direccion para comprar los productos, cargarlo en el carrito
   }
 
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';   
- 
+
 function Main() {   
     return (   
         <main style={{ padding: "20px" }}>   
